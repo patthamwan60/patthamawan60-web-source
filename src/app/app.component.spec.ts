72e49e55845933bf102b81b71c1259421a783077
patthamawan60 <patthamawan.so.60@ubu.ac.th>
2019-09-13 13:34:33 +0700

    add

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('website');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to website!');
+    expect(compiled.querySelector('.content span').textContent).toContain('website app is running!');
   });
 });
